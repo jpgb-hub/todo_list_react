cd263a07bb2eabe380b5e2f8b06963c213c3fad7
jpgb-hub <jpgbonnet@gmail.com>
2023-07-28 22:52:11 -0400

    save

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,82 +1,53 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { FaTrashAlt } from "react-icons/fa";
-import axios from 'axios';
 
+// Create your first component
 const Home = () => {
-    const [inputValue, setInputValue] = useState("");
-    const [pendientes, setPendientes] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [pendientes, setPendientes] = useState([]);
 
-    const handleInputChange = (e) => {
-        setInputValue(e.target.value);
-    };
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
 
-    const fetchPendientes = async () => {
-        try {
-            const response = await axios.get("https://playground.4geeks.com/apis/fake/todos/user/<username>");
-            setPendientes(response.data);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    };
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      if (inputValue.trim() !== "") {
+        setPendientes((prevPendientes) => [...prevPendientes, inputValue]);
+        setInputValue("");
+      }
+    }
+  };
 
-    useEffect(() => {
-        fetchPendientes();
-    }, []);
-
-    const addPendiente = async () => {
-        if (inputValue.trim() !== "") {
-            const newPendiente = {
-                label: inputValue,
-                done: false
-            };
-            try {
-                await axios.post("https://playground.4geeks.com/apis/fake/todos/user/<username>", [newPendiente]);
-                fetchPendientes();
-                setInputValue("");
-            } catch (error) {
-                console.error('Error adding todo:', error);
-            }
-        }
-    };
-
-    const deletePendientes = async () => {
-        try {
-            await axios.delete("https://playground.4geeks.com/apis/fake/todos/user/<username>");
-            fetchPendientes();
-        } catch (error) {
-            console.error('Error deleting todos:', error);
-        }
-    };
-
-    const handleKeyPress = (e) => {
-        if (e.key === "Enter") {
-            addPendiente();
-        }
-    };
-
-    return (
-        <div className="container">
-            <h1>Mi lista de Pendientes</h1>
-            <ul>
-                <li>
-                    <input
-                        type="text"
-                        onChange={handleInputChange}
-                        value={inputValue}
-                        onKeyPress={handleKeyPress}
-                        placeholder="¿Qué es lo que tienes que hacer?"
-                    />
-                </li>
-                {pendientes.map((pendiente, i) => (
-                    <li key={i}>
-                        {pendiente.label}
-                        <FaTrashAlt onClick={deletePendientes} />
-                    </li>
-                ))}
-            </ul>
-            <div>{pendientes.length} tarea(s)</div>
-        </div>
+  const deletePendiente = (index) => {
+    setPendientes((prevPendientes) =>
+      prevPendientes.filter((_, i) => i !== index)
     );
+  };
+
+  return (
+    <div className="container">
+      <h1>Mi lista de Pendientes</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            onChange={handleInputChange}
+            value={inputValue}
+            onKeyPress={handleKeyPress}
+            placeholder="¿Qué es lo que tienes que hacer?"
+          />
+        </li>
+        {pendientes.map((pendiente, index) => (
+          <li key={index}>
+            {pendiente}
+            <FaTrashAlt onClick={() => deletePendiente(index)} />
+          </li>
+        ))}
+      </ul>
+      <div>{pendientes.length} tarea(s)</div>
+    </div>
+  );
 };
 
 export default Home;
